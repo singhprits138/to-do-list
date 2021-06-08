6f9ac90307c85e823b849bff7dd0e83c75b27c1b
Sadhna Singh <sadhna.s@fyntune.com>
2021-06-08 19:37:14 +0530

    To_do_list react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
 
-function App() {
+const App = () =>{
+  const [inputList,setInputList] = useState("");
+  const [itmes,setItems] = useState([]);
+  const itemEvent = (event) =>{
+    setInputList(event.target.value);
+  }
+  const listofItems = () =>{
+    setItems((oldItems) =>{
+      return [...oldItems,inputList];
+    });
+    setInputList("");
+  };
+  const deleteItems = (id)=>{
+    console.log("deleted");
+    setItems((oldItems)=>{
+      return oldItems.filter((arrayElem,index)=>{
+        return index !== id;
+      });
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+          <div className="center_div">
+              <br/>
+              <h1>TO DO List</h1>
+              <br/>
+              <input type="text" className="add_item" placeholder="Add an item" value={inputList} onChange={itemEvent}/>
+              <button className="add_btn" onClick={listofItems}>+</button>
+              <ol>{itmes.map((val,index)=>{
+                return <ToDoList 
+                key ={index}
+                id={index}
+                text={val}
+                onSelect={deleteItems}/>;
+              })
+                }
+              </ol>
+          </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/ToDoList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ToDOList = (props) =>{
+   
+    return (
+        <>
+            <div className="to_do_style">
+                <i className='fa fa-times' 
+                aria-hidden="true" 
+                onClick={()=>{
+                    props.onSelect(props.id);
+                }} />
+                <li key={props.text}>{props.text}</li>
+            </div>
+        </>
+    
+    );
+    
+};
+
+export default ToDOList;
